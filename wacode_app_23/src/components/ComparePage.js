717a96f33ecd8e201ca33c57454529c35fba787d
mr-noah <noah134567@gmail.com>
2023-04-15 18:44:51 -0500

    compare page

--- wacode_app_23/src/components/ComparePage.js
+++ wacode_app_23/src/components/ComparePage.js
@@ -1,12 +1,40 @@
-import  React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { TextField, List, ListItem, ListItemText } from '@mui/material/';
+import { db } from '../config/firebase';
 
 function ComparePage() {
-    return (
-      <div className="">
-        compare page
-      </div>
-    );
-  }
-  
-  export default ComparePage;
+  const [results, setResults] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = async () => {
+    const q = query(collection(db, 'users'), where('city', '==', searchValue));
+    const querySnapshot = await getDocs(q);
+    const queryResults = querySnapshot.docs.map((doc) => doc.data());
+    setResults(queryResults);
+  };
+
+  return (
+    <div style={{ paddingTop: '50px' }}>
+      <TextField
+        label="Search by city"
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            handleSearch();
+          }
+        }}
+      />
+      <List>
+        {results.map((result, index) => (
+          <ListItem key={index}>
+            <ListItemText primary={result.name} secondary={result.city} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default ComparePage;
