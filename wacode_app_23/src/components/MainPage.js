899116bacaf46a6950a9ad4a2f99ee9df5bc7839
RichardHutcheson <richardhutch80@gmail.com>
2023-04-15 11:43:12 -0500

    add material and tailwindcss

---
+++ wacode_app_23/src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Create a functional component
+const MainPage = (props) => {
+    return (
+      <div className="bg-blue-100 p-4 rounded-md shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Hello, World!</h1>
+        <p className="text-lg">This is a functional component using ReactJS and Tailwind CSS.</p>
+      </div>
+    );
+  };
+  
+  // Export the component
+  export default MainPage;
