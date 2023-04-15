f0de432084aa6bb489e507300767859fdfaa1b48
RichardHutcheson <richardhutch80@gmail.com>
2023-04-15 13:30:06 -0500

    add material ui

--- wacode_app_23/src/components/DailyInputForm.js
+++ wacode_app_23/src/components/DailyInputForm.js
@@ -4,7 +4,6 @@ import React from 'react';
 const DailyInputForm = (props) => {
     return (
         // <div className="flex h-3/4 items-center justify-center bg-gray-100">
-        
         <div className="h-3/4 w-3/4 flex justify-center bg-gray-100 rounded-3xl drop-shadow-xl">
             <div className="">form title</div>
         </div>
--- wacode_app_23/src/components/MainPage.js
+++ wacode_app_23/src/components/MainPage.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import DailyInputForm from './DailyInputForm';
+import React from "react";
+import DailyInputForm from "./DailyInputForm";
+import MenuIcon from '@mui/icons-material/Menu';
+
 // Create a functional component
 const MainPage = (props) => {
-    return (
-<div class="h-screen w-screen flex items-center justify-center">
-        <DailyInputForm/>
-      </div>
+  return (
+    <div class="h-screen w-screen flex items-center justify-center">
+      <MenuIcon className="absolute top-5 left-5"/>
+      <DailyInputForm />
+    </div>
+  );
+};
 
-    );
-  };
-  
-  // Export the component
-  export default MainPage;
+// Export the component
+export default MainPage;
