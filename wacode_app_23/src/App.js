da56f2b48a8db7969512ee24691c4358dadc184b
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2023-04-15 16:26:50 -0500

    pushing my sea level rise code

--- wacode_app_23/src/App.js
+++ wacode_app_23/src/App.js
@@ -7,9 +7,9 @@ import MenuIcon from '@mui/icons-material/Menu';
 function App() {
   return (
     <div className="App-backgroundMain">
-      <div className="App font-mono">
+      {/* <div className="App font-mono">
           <LoginMenu/>
-      </div>
+      </div> */}
       <div class="h-screen w-screen flex items-center justify-center">
         <MenuIcon className="absolute top-5 left-5"/>
         <DailyInputForm />
