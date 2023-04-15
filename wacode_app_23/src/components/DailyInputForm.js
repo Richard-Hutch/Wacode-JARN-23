87027a595cc407ab1761df01c78f5bae44816e1d
RichardHutcheson <richardhutch80@gmail.com>
2023-04-15 13:10:27 -0500

    add app font, working on main page

---
+++ wacode_app_23/src/components/DailyInputForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Create a functional component
+const DailyInputForm = (props) => {
+    return (
+        // <div className="flex h-3/4 items-center justify-center bg-gray-100">
+        
+        <div className="h-3/4 w-3/4 flex justify-center bg-gray-100 rounded-3xl drop-shadow-xl">
+            <div className="">form title</div>
+        </div>
+    );
+  };
+  
+  // Export the component
+  export default DailyInputForm;
--- wacode_app_23/src/components/MainPage.js
+++ wacode_app_23/src/components/MainPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import DailyInputForm from './DailyInputForm';
 // Create a functional component
 const MainPage = (props) => {
     return (
-      <div className="bg-blue-100 p-4 rounded-md shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Hello, World!</h1>
-        <p className="text-lg">This is a functional component using ReactJS and Tailwind CSS.</p>
+<div class="h-screen w-screen flex items-center justify-center">
+        <DailyInputForm/>
       </div>
+
     );
   };
   
