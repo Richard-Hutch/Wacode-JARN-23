f8dc596882462e5037799087829e39e896536edd
jackhal <jackhale@gmail.com>
2023-04-15 15:04:08 -0500

    jack's beautiful changes

--- wacode_app_23/src/App.js
+++ wacode_app_23/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import MainPage from './components/MainPage';
+import DailyInputForm from './components/DailyInputForm';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <MainPage/>
+    <div class="h-screen w-screen flex items-center justify-center">
+        <MenuIcon className="absolute top-5 left-5"/>
+        <DailyInputForm />
     </div>
   );
 }
